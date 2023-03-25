b9c186bc4a451343020a85e8d4930dc85a49ef41
Gab1n0 <85260828+Gab1n0@users.noreply.github.com>
2023-03-25 14:05:48 +0100

    added and finished menuPage component

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -13,8 +13,33 @@ export class OrderDetailsService {
       id: 1,
       foodName: "Cheeseburger",
       foodDetails: "Cheeseburger, beef patty 180g",
-      foodPrice: 200,
-      foodImg: "https://unsplash.com/de/fotos/jh5XyK4Rr3Y?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+      foodPrice: 14.90,
+      foodImg: "https://unsplash.com/de/fotos/jh5XyK4Rr3Y?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      foodAlt: "cheeseburger"
+    },
+    {
+      id: 2,
+      foodName: "Pizza Napoli",
+      foodDetails: "Pizza mit sards, olives and kapers",
+      foodPrice: 10.90,
+      foodImg: "https://unsplash.com/de/fotos/jh5XyK4Rr3Y?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      foodAlt: "Pizza"
+    },
+    {
+      id: 3,
+      foodName: "Rigatoni",
+      foodDetails: "Big Pasta pieces filled with tomato souce",
+      foodPrice: 13.90,
+      foodImg: "https://unsplash.com/de/fotos/jh5XyK4Rr3Y?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      foodAlt: "rigatoni"
+    },
+    {
+      id: 4,
+      foodName: "Ice cream",
+      foodDetails: "Ice cream, different flavours: Oreo, Strawberry, chockolate",
+      foodPrice: 4.90,
+      foodImg: "https://unsplash.com/de/fotos/jh5XyK4Rr3Y?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      foodAlt: "icecream"
     }
   ]
 }
